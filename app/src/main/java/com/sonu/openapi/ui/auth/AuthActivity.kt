af925e5d0e7525bf5013c4098744b59868b45448
sonu <skr3704@gmail.com>
2021-11-20 15:05:02 +0530

    navigation graph setup

---
+++ app/src/main/java/com/sonu/openapi/ui/auth/AuthActivity.kt
@@ -0,0 +1,13 @@
+package com.sonu.openapi.ui.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sonu.openapi.R
+
+class AuthActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
