63735474a5e440a4a7241d831f7b4de425f82098
sonu <skr3704@gmail.com>
2021-11-24 09:58:23 +0530

    main package setup correctly

--- app/src/main/java/com/sonu/openapi/di/ActivityBuildersModule.kt
+++ app/src/main/java/com/sonu/openapi/di/ActivityBuildersModule.kt
@@ -4,6 +4,10 @@ import com.sonu.openapi.di.auth.AuthFragmentBuildersModule
 import com.sonu.openapi.di.auth.AuthModule
 import com.sonu.openapi.di.auth.AuthScope
 import com.sonu.openapi.di.auth.AuthViewModelModule
+import com.sonu.openapi.di.main.MainFragmentBuildersModule
+import com.sonu.openapi.di.main.MainModule
+import com.sonu.openapi.di.main.MainScope
+import com.sonu.openapi.di.main.MainViewModelModule
 import com.sonu.openapi.ui.auth.AuthActivity
 import com.sonu.openapi.ui.main.MainActivity
 import dagger.Module
@@ -17,8 +21,10 @@ abstract class ActivityBuildersModule {
         modules = [AuthModule::class, AuthFragmentBuildersModule::class, AuthViewModelModule::class]
     )
     abstract fun contributeAuthActivity(): AuthActivity
-
-    @ContributesAndroidInjector
+    @MainScope
+    @ContributesAndroidInjector(
+        modules = [MainModule::class, MainFragmentBuildersModule::class, MainViewModelModule::class]
+    )
     abstract fun contributeMainActivity(): MainActivity
 
 }
